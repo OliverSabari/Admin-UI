189e8a3fdc50a0899457776a0df8122124f1bfd9
Sabari <sabarihiswaran555@gmail.com>
2023-04-02 10:15:25 +0530

    pagination 100% completed

--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { MEMBERS_PER_PAGE } from '../Utils/constants'
 
-const Pages = ({ membersData, currentPage,updatePageNumber }) => {
+const Pages = ({ membersData, currentPage, updatePageNumber }) => {
 
   const totalPages = []
 
@@ -16,26 +16,63 @@ const Pages = ({ membersData, currentPage,updatePageNumber }) => {
 
   } while (i <= membersData.length / MEMBERS_PER_PAGE)
 
-
   return (
     <Container>
+
       <ul className="pageNumbersDiv">
+        <li className="pageNumbersStyling">
+          <button
+            className="pageNumbersAnchor"
+            onClick={() => updatePageNumber(1)}
+            disabled={currentPage === 1 ? true : false}
+          >
+            {`<<`}
+          </button>
+        </li>
+
+        <li className="pageNumbersStyling">
+          <button
+            className="pageNumbersAnchor"
+            onClick={() => updatePageNumber(currentPage - 1)}
+            disabled={currentPage === 1 ? true : false}
+          >
+            {`<`}
+          </button>
+        </li>
+
         {totalPages.map(item => {
           return (
             <li key={item} className="pageNumbersStyling">
-              <a 
-              href="!#" 
-              role="button" 
-              className={`pageNumbersAnchor ${currentPage === item ? "activePage" : ""}`}
-              onClick={() =>updatePageNumber(item) }
-              > 
-              {item}
-              </a>
+              <button
+                className={`pageNumbersAnchor ${currentPage === item ? "activePage" : ""}`}
+                onClick={() => updatePageNumber(item)}
+              >
+                {item}
+              </button>
             </li>
           )
         })}
-      </ul>
 
+        <li className="pageNumbersStyling">
+          <button
+            className="pageNumbersAnchor"
+            onClick={() => updatePageNumber(currentPage + 1)}
+            disabled={currentPage === totalPages.length ? true : false}
+          >
+            {`>`}
+          </button>
+        </li>
+
+        <li className="pageNumbersStyling">
+          <button
+            className="pageNumbersAnchor"
+            onClick={() => updatePageNumber(totalPages.length)}
+            disabled={currentPage === totalPages.length ? true : false}
+          >
+            {`>>`}
+          </button>
+        </li>
+      </ul>
 
     </Container>
   )
