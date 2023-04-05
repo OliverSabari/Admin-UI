8e0e883801e7fa72325b93bca1721441df418d78
Sabari <sabarihiswaran555@gmail.com>
2023-04-05 18:05:12 +0530

    fetch users using redux inprogress

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,21 +5,25 @@ import {  useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchMembers } from '../Utils/fetchMembersSlice';
 import { MEMBERS_API } from '../Utils/constants';
+import useFetchMembers from '../Utils/useFetchMembers';
 
 
 const Body = () => {
+
+  // const selector = useFetchMembers()
+
   const dispatch = useDispatch()
 
-  const selector = useSelector((store) => store.fetchMembersSlice)
- console.log("selector is " +selector.membersData)
+  const selector = useSelector((store) =>store.fetchMembersSlice)
+  // console.log("selectore is " + selector)
   useEffect(() => {
     dispatch(fetchMembers(MEMBERS_API))
      
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
+  return selector.length > 0 ? (
     <SearchBar membersData={selector} /> 
-  )
+  ): ""
 }
 
 export default Body
--- src/Utils/fetchMembersSlice.js
+++ src/Utils/fetchMembersSlice.js
@@ -3,16 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const fetchMembersSlice = createSlice({
     name : "fetchMembersSlice",
-    initialState : {
-        membersData : []
-    },
+    initialState : [],
     reducers : {
         fetchMembers : async (state,action) =>{
+            
             const apiData =await fetch(action.payload)
             const jsonData = await apiData.json()
-            state.membersData = jsonData
-        
-            console.log(state.membersData)
+            state = jsonData
+          console.log(state)
         }
     }
 })
