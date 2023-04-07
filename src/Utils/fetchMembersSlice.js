5527b8e5ff743fdbd350ccc7f9d549c8e3f4e4c1
Sabari <sabarihiswaran555@gmail.com>
2023-04-07 20:04:48 +0530

    multiple record delete reducer function updated

--- src/Utils/fetchMembersSlice.js
+++ src/Utils/fetchMembersSlice.js
@@ -2,21 +2,27 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 
 export const fetchMembers = createAsyncThunk('fetchMembersSlice', (memberApi) => {
-    const apiData = fetch(memberApi).then(data => data.json())
 
+    const apiData = fetch(memberApi).then(data => data.json())
     return apiData
+
 })
 
 const fetchMembersSlice = createSlice({
+
     name: "fetchMembersSlice",
+
     initialState: {
         loading: false,
         membersData: [],
         error: "",
-        selectedCheckbox : []
+        selectedCheckbox: []
     },
+
     reducers: {
+
         addNewMembersData: (state, action) => {
+
             const newMembersState = state.membersData?.map(item => {
 
                 if (item.id === action.payload.id) {
@@ -26,15 +32,15 @@ const fetchMembersSlice = createSlice({
                 else {
                     return item
                 }
+
             })
 
             state = { ...state, membersData: newMembersState }
-
             return state
 
         },
-        deleteMemberData: (state, action) => {
 
+        deleteMemberData: (state, action) => {
 
             const handleDeletionFilter = (membersData) => {
                 return membersData.id !== action.payload.id
@@ -43,16 +49,16 @@ const fetchMembersSlice = createSlice({
             const nonDeletedState = state.membersData?.filter(handleDeletionFilter)
 
             state = { ...state, membersData: nonDeletedState }
-
             return state
         },
-        checkboxSelected : (state,action) => {
 
-                state = {...state ,selectedCheckbox: [...state.selectedCheckbox , action.payload]}
+        checkboxSelected: (state, action) => {
 
-                return state
+            state = { ...state, selectedCheckbox: [...state.selectedCheckbox, action.payload] }
+            return state
         },
-        checkboxRemove : (state,action) => {
+
+        checkboxRemove: (state, action) => {
 
             const handleCheckBoxFilter = (selectedCheckbox) => {
                 return !action.payload.includes(selectedCheckbox)
@@ -60,29 +66,47 @@ const fetchMembersSlice = createSlice({
 
             const newSelectedCheckbox = state.selectedCheckbox.filter(handleCheckBoxFilter)
 
-            state = {...state , selectedCheckbox : newSelectedCheckbox}
-
+            state = { ...state, selectedCheckbox: newSelectedCheckbox }
             return state
+        },
+
+        multipleRecorDelete : (state) => {
+
+            const handleDeletion = (membersData) => {
+                return !state.selectedCheckbox.includes(membersData.id)
+            }
+
+            const newMembersData = state.membersData.filter(handleDeletion)
+
+            state = { ...state, membersData: newMembersData , selectedCheckbox : [] }
+            return state
+
         }
+        
     },
+
     extraReducers: builder => {
+
         builder.addCase(fetchMembers.pending, (state) => {
             state.loading = true
         })
+
         builder.addCase(fetchMembers.fulfilled, (state, action) => {
             state.loading = false
             state.membersData = action.payload
             state.error = ""
         })
+
         builder.addCase(fetchMembers.rejected, (state, action) => {
             state.loading = false
             state.membersData = []
             state.error = action.error.message
         })
+
     }
 }
 )
 
-export const { addNewMembersData, deleteMemberData,checkboxSelected,checkboxRemove } = fetchMembersSlice.actions
+export const { addNewMembersData, deleteMemberData, checkboxSelected, checkboxRemove } = fetchMembersSlice.actions
 
 export default fetchMembersSlice.reducer
