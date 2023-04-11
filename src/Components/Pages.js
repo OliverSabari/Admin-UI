169d1d0388a619d4db1599d1ceeae4023447633b
Sabari <sabarihiswaran555@gmail.com>
2023-04-11 21:37:05 +0530

    table for members corrected

--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import { MEMBERS_PER_PAGE } from '../Utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
@@ -17,6 +16,7 @@ const Pages = ({ membersData, currentPage, updatePageNumber, searchText }) => {
     if (searchText.length > 0) {
       updatePageNumber(1)
     }
+    
   }, [searchText]) // eslint-disable-line react-hooks/exhaustive-deps
 
   const totalPages = []
--- src/Components/Tableformembers.js
+++ src/Components/Tableformembers.js
@@ -1,77 +1,92 @@
-import React, {  useState } from 'react'
-
+import React from 'react'
 import { Container, Table } from 'react-bootstrap'
 import Member from './Member'
 import { useDispatch, useSelector } from 'react-redux'
 import { addCheck } from '../Utils/allCheckSlice'
 
-const TableForMembers = ({membersData,currentPage,name}) => {
-
+const TableForMembers = ({ membersData, currentPage, name }) => {
 
     const dispatch = useDispatch()
 
     const allCheck = useSelector((store) => store.allCheckSlice)
-   
+
+    //Map and passing the each data to Members Component to display in table
     const tableData = membersData?.map(item => {
-        
+
         return (
             <Member
-            key={item?.id} 
-            id={item?.id}
-            name={item?.name}
-            email = {item?.email} 
-            role = {item?.role}
-            checked={allCheck?.[name] ? true : false}
+                key={item?.id}
+                id={item?.id}
+                name={item?.name}
+                email={item?.email}
+                role={item?.role}
+                checked={allCheck?.[name] ? true : false}
             />
         )
     })
 
 
-    const handleChange = (e) => { 
+    const handleChange = (e) => {
 
-        const {name,checked} = e.target
-
-        dispatch(addCheck({[name] : checked}))
+        const { name, checked } = e.target
+        
+        dispatch(addCheck({ [name]: checked }))
     }
 
-    if(membersData.length ===0) {
+    //If searched text does not match any records
+    if (membersData.length === 0) {
         return (
             <h4 className='noRecords'> No records matches your search . </h4>
         )
     }
 
-
     return (
-        <Container className='tableContainer'>
+
+        <Container
+            className='tableContainer'
+        >
             <Table>
+
                 <thead>
+
                     <tr>
+
                         <th>
-                            <input type="checkbox" 
-                            onChange={handleChange}
-                            name={name}
-                            checked = {allCheck?.[name] || false}
+                            <input type="checkbox"
+                                onChange={handleChange}
+                                name={name}
+                                checked={allCheck?.[name] || false}
                             />
                         </th>
+
                         <th>
                             Name
                         </th>
+
                         <th>
                             Email
                         </th>
+
                         <th>
                             Role
                         </th>
+
                         <th colSpan="2">
                             Action
                         </th>
+
                     </tr>
+
                 </thead>
 
                 <tbody>
-                        {tableData}
+
+                    {tableData}
+
                 </tbody>
+
             </Table>
+
         </Container>
     )
 }
