b385477b38e7598dbcbd374862cc1e0064b2b1ad
Sabari <sabarihiswaran555@gmail.com>
2023-04-12 21:42:18 +0530

    member component updated

--- src/Components/Member.js
+++ src/Components/Member.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
 import Edit from '../Images/EditIcon.png'
-
 import Delete from '../Images/DeleteIcon.png'
 import { useDispatch } from 'react-redux'
 import { addNewMembersData, checkboxRemove, checkboxSelected, deleteMemberData } from '../Utils/fetchMembersSlice'
@@ -14,7 +12,7 @@ const Member = ({ id, name, email, role, checked }) => {
 
   const [isEditSelected, setIsEditSelected] = useState(false)
 
-  const [cancelEdit,setCancelEdit] = useState(false) 
+  const [cancelEdit, setCancelEdit] = useState(false)
 
   const [isChecked, setIsChecked] = useState(false)
 
@@ -25,66 +23,76 @@ const Member = ({ id, name, email, role, checked }) => {
   const rowStyle = isChecked ? "dataBackground" : "noDataBackground"
 
   useEffect(() => {
-    if(checked){
+
+    if (checked) {
+
       dispatch(checkboxSelected(id))
       setIsChecked(true)
+
     }
-    else{
+
+    else {
+
       dispatch(checkboxRemove(id))
       setIsChecked(false)
+
     }
-  },[checked])
+  }, [checked])
 
   const handleEdit = () => {
+
     setCancelEdit(false)
     setIsEditSelected(true)
 
   }
 
   const handleChange = (e) => {
-      const {name,value} = e.target 
-      
-      setNewMembersData(prevVal => {
-        return (
-          {
-            ...prevVal,
-            [name] : value
-          }
-        )
-      })
+
+    const { name, value } = e.target
+
+    setNewMembersData(prevVal => {
+      return (
+        {
+          ...prevVal,
+          [name]: value
+        }
+      )
+    })
+
   }
 
   const handleEditedValue = () => {
+
     setIsEditSelected(false)
     dispatch(addNewMembersData(newMembersData))
+
   }
 
   const handleCancelEdit = () => {
+
     setCancelEdit(true)
     setIsEditSelected(false)
     setNewMembersData({ id, name, email, role })
+
   }
 
   const handleDelete = (memberDetails) => {
-   
-     if(window.confirm("Would you like to delete this record of : " + memberDetails.name)){
-      dispatch(deleteMemberData(memberDetails)) 
-     
-     } 
-     
+
+    if (window.confirm("Would you like to delete this record of : " + memberDetails.name)) {
+      dispatch(deleteMemberData(memberDetails))
+    }
+
   }
 
-  const handleCheckboxChange =(e) => {
+  const handleCheckboxChange = (e) => {
 
-    const {name,checked} = e.target
+    const { name, checked } = e.target
 
-    console.log(name + checked)
-
-    if(checked){
+    if (checked) {
       dispatch(checkboxSelected(name))
       setIsChecked(true)
     }
-    else{
+    else {
       dispatch(checkboxRemove(name))
       setIsChecked(false)
     }
@@ -97,11 +105,11 @@ const Member = ({ id, name, email, role, checked }) => {
 
       <td>
 
-        <input 
-        type="checkbox" 
-        checked={isChecked} 
-        name={id}
-        onChange={handleCheckboxChange}
+        <input
+          type="checkbox"
+          checked={isChecked}
+          name={id}
+          onChange={handleCheckboxChange}
         />
 
       </td>
@@ -109,85 +117,85 @@ const Member = ({ id, name, email, role, checked }) => {
       <td>
 
         <input
-        type="text" 
-        value={cancelEdit ? name : newMembersData.name}
-        readOnly={isReadOnly} 
-        name='name'
-        onChange={handleChange}
-        className={inputStyle}
+          type="text"
+          value={cancelEdit ? name : newMembersData.name}
+          readOnly={isReadOnly}
+          name='name'
+          onChange={handleChange}
+          className={inputStyle}
         />
 
       </td>
 
       <td>
 
-        <input 
-        type="text" 
-        value={cancelEdit ? email : newMembersData.email} 
-        readOnly={isReadOnly}
-        name='email'
-        onChange={handleChange}
-        className={`emailWidth ${inputStyle}`}
+        <input
+          type="text"
+          value={cancelEdit ? email : newMembersData.email}
+          readOnly={isReadOnly}
+          name='email'
+          onChange={handleChange}
+          className={`emailWidth ${inputStyle}`}
         />
 
       </td>
 
       <td>
 
-        <input 
-        type="text" 
-        value={cancelEdit ? role : newMembersData.role} 
-        readOnly={isReadOnly} 
-        name='role'
-        onChange={handleChange}
-        className={inputStyle}
+        <input
+          type="text"
+          value={cancelEdit ? role : newMembersData.role}
+          readOnly={isReadOnly}
+          name='role'
+          onChange={handleChange}
+          className={inputStyle}
         />
 
       </td>
 
       <td>
 
-        {isEditSelected ? 
-        <>
-        <span
-        onClick={handleEditedValue}
-        className='okayButton'
-        >
-          &#10003;
-        </span>
+        {isEditSelected ?
+          <>
+            <span
+              onClick={handleEditedValue}
+              className='okayButton'
+            >
+              &#10003;
+            </span>
 
-        <span 
-        onClick={handleCancelEdit}
-        className='cancelButton'>
-            X
-        </span>
-        </>
-        :
-        <span>
+            <span
+              onClick={handleCancelEdit}
+              className='cancelButton'>
+              X
+            </span>
+          </>
+          :
+          <span>
 
-          <img 
-          src={Edit} 
-          alt="edit"
-          className='customizeEditIcon' 
-          onClick={handleEdit}
-          />
+            <img
+              src={Edit}
+              alt="edit"
+              className='customizeEditIcon'
+              onClick={handleEdit}
+            />
 
-        </span>
-      }
-  </td> 
+          </span>
+        }
+      </td>
 
-  <td>
- 
+      <td>
+
         <span className='deleteIconSpan'>
-          <img 
-          src={Delete} 
-          alt="delete" 
-          className='customizeDeleteIcon' 
-          onClick={() => handleDelete(newMembersData)}
+          <img
+            src={Delete}
+            alt="delete"
+            className='customizeDeleteIcon'
+            onClick={() => handleDelete(newMembersData)}
           />
 
         </span>
-       
+
       </td>
 
     </tr>
